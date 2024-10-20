33c47cfd5458662607936f71926f2537fc692b65
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2024-10-20 22:43:15 +0700

    persebaran data is done

--- pages/persebaran_data.py
+++ pages/persebaran_data.py
@@ -1,14 +1,57 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import plost
+
+
 st.set_page_config(layout='wide')
 x_df = pd.read_csv('./dataset/dataset_predicted_sentiment.csv')
 top_user_df = pd.read_csv('./dataset/top_users_data.csv')
 
+with open('./css/style.css') as f :
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+def card_metric(label, value):
+    return f"""
+    <div class="css-1r6slb0 e1tzin5v2">
+        <h6>{label}</h6>
+        <p style='font-size:32px; font-weight:600'>{value}</p>
+    </div>
+    """
+
+def donut_chart(selected_theta, data1):
+    plost.donut_chart(
+        data=data1,
+        theta=selected_theta,
+        color='company',
+        legend='bottom',
+        use_container_width=True
+    )
+
 def main():
     st.title('Analisis Sentiment data tweet')
+    label_df = x_df['predict_sentiment'].value_counts().reset_index()# Mengganti nama kolom
+    st.write(label_df)
+    data = {
+        'company': ['Positif', 'Negatif', 'Netral'],
+        'jumlah kata': [label_df.iloc[0]['count'], label_df.iloc[1]['count'], label_df.iloc[2]['count']],
+    }
+    data = pd.DataFrame(data)
+
     col1, col2, col3 = st.columns(3)
 
+    with col1:
+        st.markdown(card_metric('Jumlah kata positif', label_df.iloc[0]['count']), unsafe_allow_html=True)
+    with col2:
+        st.markdown(card_metric('Jumlah kata negatif', label_df.iloc[1]['count']), unsafe_allow_html=True)
+    with col3:
+        st.markdown(card_metric('Jumlah kata netral', label_df.iloc[2]['count']), unsafe_allow_html=True)
+
+    donut_theta = 'jumlah kata'
+    donut_chart(donut_theta, data)
+
+    col1, col2, col3 = st.columns(3)
     with col1:
         st.markdown('**User dengan tweet positif terbanyak**')
         positive_count = top_user_df.groupby('username')['positive'].sum().reset_index()  # Hitung jumlah positif per username
@@ -45,6 +88,8 @@ def main():
             height=300
         )
         st.altair_chart(chart, use_container_width=True)
+
+
 
 if __name__ == '__main__':
      main()
