4ef254369d5855cdd225d5c3f099e1b7c01ac8da
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2024-10-20 21:19:31 +0700

    ke page persebaran data

--- pages/persebaran_data.py
+++ pages/persebaran_data.py
@@ -4,8 +4,16 @@ import pandas as pd
 x_df = pd.read_csv('./dataset/dataset_predicted_sentiment.csv')
 
 def main():
-    st.markdown('ini adalah halaman wordcloud')
-    my_table = st.dataframe(x_df)
+    st.title('Analisis Sentiment data tweet')
+    st.table(x_df)
+    # col1, col2, col3 = st.columns(3)
+    # with col1:
+    #     st.subheader('Jumlah Tweet Positif per User')
+    #     #st.bar_chart(x_df.set_index('username')['positive'], use_container_width=True)
+    # with col2:
+    #     st.write('iya')
+    # with col3:
+    #     st.write('iya')
 
 if __name__ == '__main__':
      main()
